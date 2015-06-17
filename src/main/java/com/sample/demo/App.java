45738c226e035739b86b1200fe21e0d63b55ebb1
Nevin Zhu <nevinzhu@Nevins-MacBook-Pro.local>
2015-06-17 12:55:24 -0400

    first committ

---
+++ src/main/java/com/sample/demo/App.java
@@ -0,0 +1,21 @@
+package com.sample.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String appendRes (String in) {
+		return (in + "  Hello World");
+	}
+	
+	public String transformRes (String in) {
+		return ("OK!");
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
